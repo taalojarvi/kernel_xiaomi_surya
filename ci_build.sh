175f560a49846e0af3f6409553c2edb45716bc2a
David112x <86davidx64@gmail.com>
2024-05-02 21:02:48 -0500

    CI: add branch name to kernel zip
    Should make it easier to keep track of things.
    
    Signed-off-by: David112x <86davidx64@gmail.com>

--- ci_build.sh
+++ ci_build.sh
@@ -109,7 +109,7 @@ cd AnyKernel3
 zip -r9 UPDATE-AnyKernel3.zip * -x README.md LICENSE UPDATE-AnyKernel3.zip zipsigner.jar
 cp UPDATE-AnyKernel3.zip package.zip 
 curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
-java -jar zipsigner-3.0.jar UPDATE-AnyKernel3.zip Sunscape-$BUILD_ID.zip
+java -jar zipsigner-3.0.jar UPDATE-AnyKernel3.zip Sunscape-$GITBRNCH-$BUILD_ID.zip
 BUILD_END=$(date +"%s")
 DIFF=$((BUILD_END - BUILD_START))
-tg_post_build "Sunscape-$BUILD_ID.zip" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+tg_post_build "Sunscape-$GITBRNCH-$BUILD_ID.zip" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
